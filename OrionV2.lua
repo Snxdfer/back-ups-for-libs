9497bf9f1857478804250912eedd4eb5b790b15c
00Fazee <121313122+Snxdfer@users.noreply.github.com>
2024-12-22 12:17:11 -0300

    Update OrionV2.lua

--- OrionV2.lua
+++ OrionV2.lua
@@ -13,9 +13,9 @@ local OrionLib = {
 	Themes = {
 		Default = {
 			Main = Color3.fromRGB(25, 25, 25),
-			Second = Color3.fromRGB(32, 32, 32),
+			Second = Color3.fromRGB(12, 68, 179),
 			Stroke = Color3.fromRGB(60, 60, 60),
-			Divider = Color3.fromRGB(12, 68, 179),
+			Divider = Color3.fromRGB(60, 60, 60),
 			Text = Color3.fromRGB(240, 240, 240),
 			TextDark = Color3.fromRGB(150, 150, 150)
 		}
