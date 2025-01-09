18a2c8457a18ec80c042db46a6438e1d82c5e2d4
00Fazee <121313122+Snxdfer@users.noreply.github.com>
2025-01-09 13:33:55 -0300

    Update Luna_Source.lua

--- Luna_Source.lua
+++ Luna_Source.lua
@@ -1606,6 +1606,7 @@ end
 
 function tween(object, goal, callback, tweenin)
     local tweenInfo = tweenin or TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
+    if object:IsA("Frame") or object:IsA("TextLabel") or object:IsA("ImageLabel") or object:IsA("TextButton") or object:IsA("ImageButton") then
         local tween = TweenService:Create(object, tweenInfo, goal)
         tween.Completed:Connect(callback or function() end)
         tween:Play()
